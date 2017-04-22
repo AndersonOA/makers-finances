ec0f0a0e2b11b8201509e30b3ca7127579cf4ad8
andersonaristides <andersonaristides@localhost>
2017-04-22 17:41:49 -0300

    commit inicial do projeto

---
+++ mw-finances/src/main/java/br/com/makersweb/finances/web/MwFinancesApplication.java
@@ -0,0 +1,12 @@
+package br.com.makersweb.finances.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwFinancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MwFinancesApplication.class, args);
+	}
+}
